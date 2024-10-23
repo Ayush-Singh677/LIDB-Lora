c38908ca9b9540a6db8bdd17765bd3feed3ef90f
Ayush-Singh677 <82378979+Ayush-Singh677@users.noreply.github.com>
2024-10-23 06:05:48 +0530

    Update lidb.py

--- lidb.py
+++ lidb.py
@@ -1,6 +1,5 @@
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,42 +11,34 @@ class LoRALayer(nn.Module):
         
         self.alpha = alpha
         self.Aaux = nn.Parameter(torch.randn(in_dim, a_dim), requires_grad=False)
-        self.Aux_init(self.Aaux)
-        
-        self.Atrain = nn.Parameter(torch.zeros(a_dim, lora_dim))
-        nn.init.kaiming_uniform_(self.Atrain, a=math.sqrt(5))
-        
-        self.Btrain = nn.Parameter(torch.zeros(lora_dim, b_dim))
-        nn.init.kaiming_uniform_(self.Btrain, a=math.sqrt(5))
-        
+        # self.Aux_init(self.Aaux)
+        self.Atrain = nn.Parameter(torch.randn(a_dim, lora_dim))
+        self.Btrain = nn.Parameter(torch.randn(lora_dim, b_dim))
+        # nn.init.kaiming_uniform_(self.Btrain, a=math.sqrt(5))
         self.Baux = nn.Parameter(torch.randn(b_dim, out_dim), requires_grad=False)
-        self.Aux_init(self.Baux)
+        # self.Aux_init(self.Baux)
 
     def Aux_init(self, tensor):
         nn.init.orthogonal_(tensor)
 
     def forward(self, x):
+        A = self.Aaux @ (self.Atrain @ self.Btrain) @ self.Baux
+        return x @ A
 
-        A = self.Aaux @ self.Atrain
-        B = self.Btrain @ self.Baux
-        y = x @ A @ B
-
-        return y
-
-class LinearWithDoRA(torch.nn.Module):
+class Lidb(torch.nn.Module):
     def __init__(self, linear, lora_dim, a_dim, b_dim, alpha):
         super().__init__()
         self.linear = linear
         self.lora = LoRALayer(
             linear.in_features, linear.out_features, lora_dim, a_dim, b_dim, alpha
         )
-        
         self.m = nn.Parameter(
-            self.linear.weight.norm(p=2, dim=0, keepdim=True))
+            self.linear.weight.norm(p=2, dim=0, keepdim=True)
+        )
 
     def forward(self, x):
-        lora = self.lora.Aaux @ self.lora.Atrain @ self.lora.Btrain @ self.lora.Baux
-        combined_weight = self.linear.weight + self.lora.alpha*lora.T
+        lora_weight = self.lora.forward(x)
+        combined_weight = self.linear.weight + self.lora.alpha * lora_weight.T
         column_norm = combined_weight.norm(p=2, dim=0, keepdim=True)
         V = combined_weight / column_norm
         new_weight = self.m * V
